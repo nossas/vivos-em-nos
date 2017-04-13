4acdb0b1b7cc12db1db4a4af45468b0a5336011c
Lucas Pirola <lucaspirola@gmail.com>
2017-04-13 10:59:09 -0300

    Add todo endpoint

--- src/views/index.js
+++ src/views/index.js
@@ -1,24 +1,30 @@
 import { h } from 'preact'
 import { Router } from 'preact-router';
+import { Provider } from 'preact-redux';
 
+import store from '../store';
 import Home from './pages/home';
 import Layout from './tags/layout';
 import Article from './pages/article';
 import Error404 from './pages/errors/404';
 import Credit from './pages/credit';
 import Blog from './pages/blog';
+import Todo from './pages/todo';
 
 // track pages on route change
 const onChange = obj => window.ga && ga('send', 'pageview', obj.url);
 
 export default (
-	<Layout>
-		<Router onChange={ onChange }>
-			<Home path="/" />
-			<Blog path="/blog" />
-			<Article path="/blog/:title" />
-			<Credit path="/credit" />
-			<Error404 default />
-		</Router>
-	</Layout>
+	<Provider store={store}>
+		<Layout>
+			<Router onChange={ onChange }>
+				<Home path="/" />
+				<Todo path="/todo" />
+				<Blog path="/blog" />
+				<Article path="/blog/:title" />
+				<Credit path="/credit" />
+				<Error404 default />
+			</Router>
+		</Layout>
+	</Provider>
 );

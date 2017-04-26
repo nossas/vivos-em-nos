0a3660fdd04be7ac63ef5d032dfdf4fa22cbec39
Lucas Pirola <lucaspirola@gmail.com>
2017-04-26 20:24:56 -0300

    Add babel options to webpack config

--- config/webpack.js
+++ config/webpack.js
@@ -1,59 +1,61 @@
-const { join } = require('path');
-const ExtractText = require('extract-text-webpack-plugin');
-const setup = require('./setup');
+const { join } = require('path')
+const ExtractText = require('extract-text-webpack-plugin')
+const setup = require('./setup')
+const babel = require('./babel')
+const dist = join(__dirname, '../dist')
+const exclude = /(node_modules\/(?!preact-material-components)|bower_components)/
 
-const dist = join(__dirname, '../dist');
-const exclude = /(node_modules|bower_components)/;
+module.exports = (env) => {
+  const isProd = env && env.production
 
-module.exports = env => {
-	const isProd = env && env.production;
-
-	return {
-		entry: {
-			app: './src/index.js',
-			vendor: [
-				// pull these to a `vendor.js` file
-				'preact'
-			]
-		},
-		output: {
-			path: dist,
-			filename: '[name].[hash].js',
-			publicPath: '/'
-		},
-		resolve: {
-			alias: {
-				// you may need `preact-compat` instead!
-				'react': 'preact-compat',
-				'react-dom': 'preact-compat'
-			}
-		},
-		module: {
-			rules: [{
-				test: /\.jsx?$/,
-				exclude: exclude,
-				loader: 'babel-loader'
-			}, {
-				test: /\.(sass|scss)$/,
-				loader: isProd ? ExtractText.extract({
-					fallbackLoader: 'style-loader',
-					loader: 'css-loader!postcss-loader!sass-loader'
-				}) : 'style-loader!css-loader!postcss-loader!sass-loader'
-			}]
-		},
-		plugins: setup(isProd),
-		devtool: !isProd ?
-			// Produces an external source map (lives next to bundle output files).
-      'source-map'
-      // Produces no source map.
-      : 'hidden-source-map',
-		devServer: {
-			contentBase: dist,
-			port: process.env.PORT || 3000,
-			historyApiFallback: true,
-			compress: isProd,
-			inline: !isProd,
-			hot: !isProd
-		}
-	};
-};
+  return {
+    entry: {
+      app: './src/index.js',
+      vendor: [
+        // pull these to a `vendor.js` file
+        'preact'
+      ]
+    },
+    output: {
+      path: dist,
+      filename: '[name].[hash].js',
+      publicPath: '/'
+    },
+    resolve: {
+      alias: {
+        // you may need `preact-compat` instead!
+        'react': 'preact-compat',
+        'react-dom': 'preact-compat'
+      }
+    },
+    module: {
+      rules: [{
+        test: /\.(js|jsx)$/,
+        exclude: exclude,
+        loader: 'babel-loader',
+        options: babel
+      },
+      {
+        test: /\.(sass|scss)$/,
+        loader: isProd ? ExtractText.extract({
+          fallbackLoader: 'style-loader',
+          loader: 'css-loader!postcss-loader!sass-loader'
+        }) : 'style-loader!css-loader!postcss-loader!sass-loader'
+      }]
+    },
+    plugins: setup(isProd),
+    devtool: !isProd ?
+    // Produces an external source map (lives next to bundle output files).
+    'source-map'
+    // Produces no source map.
+    : 'hidden-source-map',
+    devServer: {
+      contentBase: dist,
+      port: process.env.PORT || 3000,
+      historyApiFallback: true,
+      compress: isProd,
+      inline: !isProd,
+      hot: !isProd
+    }
+  }
+}

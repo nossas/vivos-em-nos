7f2c6665c757b0c349ca6eb1ecea4bdcac2573d9
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2017-04-27 04:49:35 -0300

    Add slide animation to Menu component close #1

--- src/menu/components/menu.js
+++ src/menu/components/menu.js
@@ -1,12 +1,9 @@
 import { h } from 'preact' /** @jsx h */
 import MenuClose from './menu-close'
 
-export default ({ children, active, setActive, ...props }) => {
-  console.log('props', props)
-  return !active ? null : (
-    <nav className="components--menu">
-      <MenuClose onClick={() => setActive(false)} />
-      {children}
-    </nav>
-  )
-}
+export default ({ children, active, setActive }) => (
+  <nav className={`components--menu ${active ? 'active' : ''}`}>
+    <MenuClose onClick={() => setActive(false)} />
+    {children}
+  </nav>
+)

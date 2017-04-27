48f3287047c48828771624927b5e27bf2f25cdd1
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2017-04-27 19:06:01 -0300

    feat(layout): Add the victim data section #8

--- src/views/pages/memory-victim.js
+++ src/views/pages/memory-victim.js
@@ -1,13 +1,27 @@
 import { h } from 'preact' /** @jsx h */
 import { Header, LayoutDefault } from '../tags/layout'
-import { TopBar } from '../components'
+import { TopBar, MemoryImage, MemorySummary } from '../components'
 
 export default () => (
   <LayoutDefault>
     <Header>
       <TopBar />
     </Header>
+
     <div className='page pages--memory-victim'>
+      <section className='section--victim-data'>
+        <MemoryImage
+          source='http://bit.ly/2proUwJ'
+          width='223px'
+          height='179px'
+        />
+        <MemorySummary
+          name='Marina'
+          birthYear='1990'
+          deathYear='2015'
+          description='Baleada em um assalto quando saía da faculdade.'
+        />
+      </section>
       victim memory page
     </div>
   </LayoutDefault>

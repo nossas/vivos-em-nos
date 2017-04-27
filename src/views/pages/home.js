c79c2adc0fe53a82a54f5a5fe7e462785bed7e4d
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2017-04-27 07:24:10 -0300

    feat(layout): Initial static featured memories #5

--- src/views/pages/home.js
+++ src/views/pages/home.js
@@ -17,6 +17,48 @@ const Home = ({ setMenuActive }) => (
         em mobilização e, juntos, lutar por mais respeito à vida. </p>
       <ButtonPrimary text="crie sua homenagem" href="/homenagem" />
     </div>
+
+    <section>
+      <header>
+        Veja algumas homenagens
+        <img className='silhouette' src='/img/silhouette-02.svg' height='380' />
+      </header>
+      <article>
+        <div className='ornament' />
+        <div
+          className='memory-image'
+          style={{ backgroundImage: 'url(http://static.quizur.com/i/b/581f5c66877f10.71342558581f5c66777f49.87913318.jpg)' }}
+        />
+        <div className='memory-summary'>
+          <div className='memory-identity'>
+            <div className='name'>Marina</div>
+            <span>1990 / 2015</span>
+          </div>
+          <div className='memory-description'>
+            <span>Baleada em um assalto quando saía da faculdade.</span>
+          </div>
+        </div>
+
+        <div className='quote'>
+          Minha filha sonhava em ser médica, em trabalhar com crianças. Pra gente aqui é tudo
+          muito difícil, mas ela trabalhava de dia e conseguia pagar a faculdade à noite. Sempre
+          foi muito batalhadora...
+        </div>
+      </article>
+      <footer>
+        <div className='memory-carousel-navigator'>
+          <div className='carousel-item active' />
+          <div className='carousel-item' />
+          <div className='carousel-item' />
+          <div className='carousel-item' />
+          <div className='carousel-item' />
+        </div>
+        <ButtonPrimary
+          text="ver homenagem completa"
+          href="/homenagem"
+        />
+      </footer>
+    </section>
   </div>
 )
 

b74b467d749d7d6b2c05b665f3ee23e8d64c16ef
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2017-04-30 16:22:41 -0300

    feat(components): twitter share button action strategy #12

--- src/views/components/share-twitter-button.js
+++ src/views/components/share-twitter-button.js
@@ -1,21 +1,42 @@
 import { h } from 'preact' /** @jsx h */
+import * as detect from '../../utils/detect'
+
+
+const Button = ({ children, href, text, ...props }) => (
+  <button
+    {...props}
+    onClick={() => {
+      window.open(
+        `https://twitter.com/intent/tweet?text=${text}&url=${href}`,
+        'Compartilhar no Twitter',
+        'width=800,height=600',
+      )
+    }}
+  >
+    {children}
+  </button>
+)
+
+const Anchor = ({ children, href, text, ...props }) => (
+  <a
+    {...props}
+    href={`twitter://post?message=${text}${href}`}
+  >
+    {children}
+  </a>
+)
 
 export default ({ href, text, className }) => {
-  const handleClick = () => {
-    window.open(
-      `https://twitter.com/intent/tweet?text=${encodeURIComponent(text)}&url=${href}`,
-      'Compartilhar no Twitter',
-      'width=800,height=600',
-    )
-  }
+  const Component = detect.mobile ? Anchor : Button
 
   return (
-    <button
+    <Component
       title="Compartilhar no Twitter"
       className={className}
-      onClick={handleClick}
+      href={href}
+      text={encodeURIComponent(text)}
     >
       <img src="/img/share-twitter.svg" width="51" />
-    </button>
+    </Component>
   )
 }

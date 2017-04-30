e47bdc31ef334c7f4bf09f186bff7d18263a8135
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2017-04-30 13:51:32 -0300

    feat(layout): add create memory route on menu item #9

--- src/views/layout/layout.js
+++ src/views/layout/layout.js
@@ -1,25 +1,24 @@
 import { h } from 'preact' /** @jsx h */
 import { Layout, Section } from 'preact-layout'
 import { Menu, MenuItem } from '../../menu/components'
+import * as paths from '../../paths'
 
 const Header = () => {}
 
-const LayoutDefault = ({ children }) => {
-  return (
-    <Layout>
-      <Menu>
-        <MenuItem href='/'>Início</MenuItem>
-        <MenuItem>Criar Homenagem</MenuItem>
-        <MenuItem>Editar Homenagem</MenuItem>
-        <MenuItem>Quem Somos</MenuItem>
-      </Menu>
-      <Section type={Header} />
-      <Section>{children}</Section>
-    </Layout>
+const LayoutDefault = ({ children }) => (
+  <Layout>
+    <Menu>
+      <MenuItem href={paths.home()}>Início</MenuItem>
+      <MenuItem href={paths.memoryCreate()}>Criar Homenagem</MenuItem>
+      <MenuItem>Editar Homenagem</MenuItem>
+      <MenuItem>Quem Somos</MenuItem>
+    </Menu>
+    <Section type={Header} />
+    <Section>{children}</Section>
+  </Layout>
   )
-}
 
 export {
   Header,
-  LayoutDefault
+  LayoutDefault,
 }

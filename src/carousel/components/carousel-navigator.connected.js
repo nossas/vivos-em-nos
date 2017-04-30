13ec818cbddd47bd31ff6270fc59bc6cac8f66fc
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2017-04-30 09:00:13 -0300

    refactor(examples): remove unused files #9

--- src/carousel/components/carousel-navigator.connected.js
+++ src/carousel/components/carousel-navigator.connected.js
@@ -2,11 +2,11 @@ import { connect } from 'preact-redux'
 import CarouselNavigator from './carousel-navigator'
 import CarouselSelectors from '../redux/selectors'
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   const selectors = CarouselSelectors(state)
   return {
     list: selectors.getList(),
-    currentIndex: selectors.getCurrentIndex()
+    currentIndex: selectors.getCurrentIndex(),
   }
 }
 
--- src/carousel/components/carousel-navigator.js
+++ src/carousel/components/carousel-navigator.js
@@ -1,16 +1,14 @@
 import { h } from 'preact' /** @jsx h */
 
-export default ({ list, currentIndex }) => {
-  return (
-    <div>
-      <div className='memory-carousel-navigator'>
-        {list.map((item, index) => {
-          const active = index === currentIndex ? 'active' : ''
-          return (
-            <div className={`carousel-item ${active}`} />
-          )
-        })}
-      </div>
+export default ({ list, currentIndex }) => (
+  <div>
+    <div className="memory-carousel-navigator">
+      {list.map((item, index) => {
+        const active = index === currentIndex ? 'active' : ''
+        return (
+          <div className={`carousel-item ${active}`} />
+        )
+      })}
     </div>
-  )
-}
+  </div>
+)

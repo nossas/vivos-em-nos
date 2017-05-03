eb46f25ebfb4dd37a9c8b2e8f4315b5a76c1528b
Igor Santos <igr.exe@gmail.com>
2017-05-03 10:07:38 -0300

    add loading to create memory page when load by token

--- src/memories/pages/memory-create/page.connected.js
+++ src/memories/pages/memory-create/page.connected.js
@@ -1,6 +1,23 @@
 import { connect } from 'preact-redux'
 import { graphql } from 'react-apollo'
+import { hoc } from '../../../utils/preact'
+import LoaderHOC from '../../../loader'
 import { memoryByToken } from '../../queries'
 import Page from './page'
 
-export default graphql(memoryByToken, { name: 'getMemoryByToken' })(Page)
+const mapStateToProps = (state, { getMemoryByToken: { loading, allMemories } }) => {
+  if (!loading && allMemories) {
+    if (allMemories.nodes[0]) {
+      const { memoryAssetsByMemoryId, ...memory } = allMemories.nodes[0]
+      memory.memoryAssets = [...memoryAssetsByMemoryId.nodes, {}]
+      return { memory, loading }
+    }
+  }
+  return { loading }
+}
+
+export default graphql(memoryByToken, { name: 'getMemoryByToken' })(
+  connect(mapStateToProps)(
+    hoc(LoaderHOC, Page)
+  )
+)
--- src/memories/pages/memory-create/page.js
+++ src/memories/pages/memory-create/page.js
@@ -1,47 +1,27 @@
-import { h, Component } from 'preact' /** @jsx h */
+import { h } from 'preact' /** @jsx h */
 import { Header, Footer, LayoutDefault } from '~src/views/layout/layout'
 import { TopBar, OrnamentPageFooter } from '~src/views/components'
 import { MemoryForm } from '~src/memories/components'
 
-export default class Page extends Component {
-  getMemoryByToken() {
-    let memoryByToken = {}
+export default ({ memory }) => (
+  <LayoutDefault>
+    <Header>
+      <TopBar />
+    </Header>
 
-    if (this.props.getMemoryByToken.allMemories) {
-      const { nodes } = this.props.getMemoryByToken.allMemories
-      if (nodes[0]) {
-        const { memoryAssetsByMemoryId, ...memory } = nodes[0]
-        memory.memoryAssets = [...memoryAssetsByMemoryId.nodes, {}]
-        memoryByToken = memory
-      }
-    }
-    return memoryByToken
-  }
+    <Footer />
 
-  render() {
-    const memory = this.getMemoryByToken()
-
-    return (
-      <LayoutDefault>
-        <Header>
-          <TopBar />
-        </Header>
-
-        <Footer />
-
-        <div className="page page--memory-create">
-          <p>
-            Preenchendo o formulário abaixo, você cria uma homenagem a alguém que morreu,
-            vítima da violência.
-          </p>
-          <p>
-            Depois de publicada, você vai poder compartilhar essa página nas suas redes sociais
-            e dizer a todos como essa pessoa permanece viva na sua memória.
-          </p>
-          <MemoryForm memory={memory} />
-          <OrnamentPageFooter />
-        </div>
-      </LayoutDefault>
-    )
-  }
-}
+    <div className="page page--memory-create">
+      <p>
+        Preenchendo o formulário abaixo, você cria uma homenagem a alguém que morreu,
+        vítima da violência.
+      </p>
+      <p>
+        Depois de publicada, você vai poder compartilhar essa página nas suas redes sociais
+        e dizer a todos como essa pessoa permanece viva na sua memória.
+      </p>
+      <MemoryForm memory={memory} />
+      <OrnamentPageFooter />
+    </div>
+  </LayoutDefault>
+)

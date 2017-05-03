f5b314aff9f257c6225ecf5407a1aaeb5c7cbc97
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2017-05-03 14:11:26 -0300

    fix(memory-comments): share button style on desktop #95

--- src/memories/components/memory-comments-form.js
+++ src/memories/components/memory-comments-form.js
@@ -9,7 +9,6 @@ export default class MemoryCommentsForm extends Component {
 
     return (
       <Form
-        className="columns is-multiline"
         error={error}
         onSubmit={handleSubmit((...args) =>
           this.props.onSave(...args)
@@ -21,35 +20,37 @@ export default class MemoryCommentsForm extends Component {
             }),
         )}
       >
-        <Field
-          name="memoryId"
-          type="hidden"
-          component="input"
-        />
-        <Field
-          label="Texto"
-          name="comment"
-          ref={(comment) => { this.comment = comment }}
-          type="text"
-          component={TextField}
-          formGroupClassName="column is-12"
-        />
-        <Field
-          label="Nome completo"
-          name="name"
-          ref={(name) => { this.name = name }}
-          type="text"
-          component={TextField}
-          formGroupClassName="column is-half-desktop"
-        />
-        <Field
-          label="Email"
-          name="email"
-          ref={(email) => { this.email = email }}
-          type="email"
-          component={TextField}
-          formGroupClassName="column is-half-desktop"
-        />
+        <div className="columns is-multiline">
+          <Field
+            name="memoryId"
+            type="hidden"
+            component="input"
+          />
+          <Field
+            label="Texto"
+            name="comment"
+            ref={(comment) => { this.comment = comment }}
+            type="text"
+            component={TextField}
+            formGroupClassName="column is-12"
+          />
+          <Field
+            label="Nome completo"
+            name="name"
+            ref={(name) => { this.name = name }}
+            type="text"
+            component={TextField}
+            formGroupClassName="column is-half-desktop"
+          />
+          <Field
+            label="Email"
+            name="email"
+            ref={(email) => { this.email = email }}
+            type="email"
+            component={TextField}
+            formGroupClassName="column is-half-desktop"
+          />
+        </div>
 
         <ButtonPrimary TagName="button" type="submit">
           Enviar comentário

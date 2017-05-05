f76952bb5c6bad2a929743cf6951ef18620b1280
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2017-05-04 21:01:14 -0300

    chore(home): style splash section #113

--- src/menu/components/menu-trigger.js
+++ src/menu/components/menu-trigger.js
@@ -1,7 +1,16 @@
 import { h } from 'preact' /** @jsx h */
 
-export default ({ setActive }) => (
-  <button className="components--menu-trigger" onClick={() => setActive(true)}>
+const MenuTrigger = ({ setActive, className }) => (
+  <button
+    className={`components--menu-trigger ${className}`}
+    onClick={() => setActive(true)}
+  >
     <img src="/img/icone-menu.svg" alt="menu" />
   </button>
 )
+
+MenuTrigger.defaultProps = {
+  className: '',
+}
+
+export default MenuTrigger
--- src/views/pages/home.js
+++ src/views/pages/home.js
@@ -13,24 +13,38 @@ import * as paths from '~src/paths'
 
 export default () => (
   <LayoutDefault>
-    <Header></Header>
+    <Header />
+
     <div className="page page__home">
-      <div className="card splash">
-        <MenuTrigger />
-        <h1 className="logo">
-          <img
-            src="/img/logo-vivos-em-nos.svg"
-            alt="logo vivos em nos"
-          />
-        </h1>
-        <p>
-          Vamos usar a memória como ferramenta para mudança. Homenageando
-          aqueles que estão #VivosEmNós, podemos transformar saudade
-          em mobilização e, juntos, lutar por mais respeito à vida.
-        </p>
-        <ButtonPrimary href={paths.memoryCreate()}>
-          Crie sua homenagem
-        </ButtonPrimary>
+      <div className="card splash columns">
+        <div className="column--hybrid column is-half-tablet">
+          <MenuTrigger className="is-hidden-tablet" />
+          <h1 className="logo">
+            <img
+              src="/img/logo-vivos-em-nos.svg"
+              alt="logo vivos em nos"
+            />
+          </h1>
+          <p>
+            Vamos usar a memória como ferramenta para mudança. Homenageando
+            aqueles que estão <span className="color--primary">#VivosEmNós</span>, podemos
+            transformar saudade em mobilização e, juntos, lutar por mais respeito à vida.
+          </p>
+          <ButtonPrimary href={paths.memoryCreate()}>
+            Crie sua homenagem
+          </ButtonPrimary>
+        </div>
+
+        <div className="column--desktop column is-block-tablet is-half-tablet">
+          <div className="menu--horizontal">
+            <div className="menu--horizontal-item active">inicio</div>
+            <div className="menu--horizontal-item">criar homenagem</div>
+            <div className="menu--horizontal-item">quem somos</div>
+          </div>
+          <div>
+            === image placeholder ===
+          </div>
+        </div>
       </div>
 
       <SectionPrimary

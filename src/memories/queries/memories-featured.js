2e1d511135e0e6c10237e8784b861ae51f40e593
Lucas Pirola <lucaspirola@gmail.com>
2017-05-06 09:31:47 -0300

    Add field to db to filter featured memories closes #124

--- src/memories/queries/memories-featured.js
+++ src/memories/queries/memories-featured.js
@@ -1,10 +1,12 @@
 import { gql } from 'react-apollo'
 
-export default gql`
-  query {
+export default gql`query {
     allMemories(
       first: 5,
-      orderBy: ID_DESC
+      orderBy: ID_DESC,
+      condition:{
+        featuredSite:true
+      }
     ) {
       nodes {
         id,

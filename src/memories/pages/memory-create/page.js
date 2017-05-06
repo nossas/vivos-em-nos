b94cc6b7831eba05bdf9d2fd6478f81a19206d40
Lucas Pirola <lucaspirola@gmail.com>
2017-05-06 05:54:40 -0300

    Improve readbility

--- src/memories/pages/memory-create/page.js
+++ src/memories/pages/memory-create/page.js
@@ -11,10 +11,8 @@ export default ({ token, memory }) => (
 
     <div className="page page--memory-create">
       <p>
-        Preenchendo o formulário abaixo, você cria uma homenagem a alguém que morreu,
-        vítima da violência.
-      </p>
-      <p>
+        <b>Preenchendo o formulário abaixo, você cria uma homenagem a alguém que morreu,
+        vítima da violência. </b>
         Depois de publicada, você vai poder compartilhar essa página nas suas redes sociais
         e dizer a todos como essa pessoa permanece viva na sua memória.
       </p>

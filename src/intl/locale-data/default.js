3de80ecf8e9d2a2051a361612a2af008707fe2fd
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2017-05-09 22:20:45 -0300

    chore(intl): translate about campaign section #165

--- src/intl/locale-data/default.js
+++ src/intl/locale-data/default.js
@@ -1,8 +1,14 @@
 export default {
   'global--brand-name': '#VivosEmNós',
 
-  'section--about-campaign.statistics': 'As estatísticas assustam, mas os números não nos permitem enxergar o mais importante:os nomes, os rostos e as histórias dessas vítimas. Tratá-las como dígitos nosrelatórios de segurança pública nos afasta da real dimensão do problema e nosimpede de cobrar soluções efetivas.',
   'section--splash.hero': 'Vamos usar a memória como ferramenta para mudança. Homenageando aqueles que estão {hashtag}, podemos transformar saudade em mobilização e, juntos, lutar por mais respeito à vida.',
+
+  'section--about-campaign.header': 'Sobre{breakLine}a campanha',
+  'section--about-campaign.paragraph-line-rounded': '{highlightBeginning} a cada quatro pessoas, uma conhece alguém que foi {highlightEnding}',
+  'section--about-campaign.paragraph-line-rounded.highlight-beginning': 'No Brasil,',
+  'section--about-campaign.paragraph-line-rounded.highlight-ending': 'assassinado.',
+  'section--about-campaign.statistics': 'As estatísticas assustam, mas os números não nos permitem enxergar o mais importante:os nomes, os rostos e as histórias dessas vítimas. Tratá-las como dígitos nosrelatórios de segurança pública nos afasta da real dimensão do problema e nosimpede de cobrar soluções efetivas.',
+  'section--about-campaign.the-goal': '{hashtag} quer usar a memória como ferramenta para transformar solidariedade em união, saudade em mobilização e indignação em força para mudança. Se você conhece alguém que teve a sua história interrompida pela violência, crie aqui uma página de homenagem para se juntar à luta por mais respeito à vida.',
 
   'components--menu-item.home': 'Início',
   'components--menu-item.create-memory': 'Criar Homenagem',
--- src/intl/locale-data/es.js
+++ src/intl/locale-data/es.js
@@ -1,8 +1,14 @@
 export default {
   'global--brand-name': '#VivosEnNosotros',
 
-  'section--about-campaign.statistics': 'Las alarmantes estadísticas, pero los números no nos permiten ver el más importante: los nombres, los rostros y las historias de estas víctimas. Tratarlos como nosrelatórios dígitos seguridad pública nos aleja de la escala real del problema y para cargar soluciones efectivas nosimpede.',
   'section--splash.hero': 'Vamos a usar la memoria como herramienta para el cambio. Homenajeando aquellos que están {hashtag}, podemos transformar la añoranza en movilización y, asimismo, luchar por más respeto a la vida.',
+
+  'section--about-campaign.header': 'Sobre{breakLine}la campaña',
+  'section--about-campaign.paragraph-line-rounded': '{highlightBeginning} en América Latina y El Caribe ocurren cuatro {highlightEnding}',
+  'section--about-campaign.paragraph-line-rounded.highlight-beginning': 'Cada 15 minutos',
+  'section--about-campaign.paragraph-line-rounded.highlight-ending': 'homicidios.',
+  'section--about-campaign.statistics': 'Las estadísticas asustan, pero los números no nos permiten ver lo más importante: los nombres, los rostros y las historias de esas víctimas. Tratarlas como números en informes de seguridad pública nos aleja de la real dimensión del problema y nos impide exigir soluciones efectivas.',
+  'section--about-campaign.the-goal': '{hashtag} quiere usar la memoria como herramienta para transformar la solidaridad en unión, la añoranza en movilización y la indignación en fuerza para el cambio. Si conoces a alguien que ha tenido su historia interrumpida por la violencia, crea aquí una página de homenaje para unirte a la lucha por más respeto a la vida.',
 
   'components--menu-item.home': 'Inicio',
   'components--menu-item.create-memory': 'Crear Homenaje',

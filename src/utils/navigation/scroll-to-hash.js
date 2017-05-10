746bd5b03edc4dda8909fb2590afd5f6045520ea
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2017-05-10 15:07:56 -0300

    fix(memory-comment): remove console.log #167

--- src/utils/navigation/scroll-to-hash.js
+++ src/utils/navigation/scroll-to-hash.js
@@ -4,7 +4,6 @@ export default () => {
   }
   if (window.location.hash) {
     setTimeout(() => {
-      console.log('window.location.hash', window.location.hash)
       const hashTarget = document.querySelector(window.location.hash)
       if (hashTarget) hashTarget.scrollIntoView()
     })

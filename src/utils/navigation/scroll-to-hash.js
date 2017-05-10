b4c3c5f7625c9eeb974947722900b92c10ed669c
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2017-05-10 15:01:29 -0300

    fix(memory-comment): scroll to section after submit #167

--- src/utils/navigation/scroll-to-hash.js
+++ src/utils/navigation/scroll-to-hash.js
@@ -2,4 +2,11 @@ export default () => {
   if (window.location.search) {
     location.href = '#about-us'
   }
+  if (window.location.hash) {
+    setTimeout(() => {
+      console.log('window.location.hash', window.location.hash)
+      const hashTarget = document.querySelector(window.location.hash)
+      if (hashTarget) hashTarget.scrollIntoView()
+    })
+  }
 }

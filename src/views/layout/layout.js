1b865578c28e004f821d10234228909d7c8c9341
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2017-05-09 21:30:42 -0300

    chore(intl): translate mobile menu to spanish #165

--- src/views/layout/layout.js
+++ src/views/layout/layout.js
@@ -1,5 +1,6 @@
 import { h } from 'preact' /** @jsx h */
 import { Layout, Section } from 'preact-layout'
+import { FormattedMessage } from 'react-intl'
 import { connect } from 'preact-redux'
 import { Loader } from '~src/loader/components'
 import LoaderSelectors from '~src/loader/redux/selectors'
@@ -18,22 +19,43 @@ const LayoutDefault = connect(mapStateToProps)(
     <Layout>
       {!isLoaderActive ? <div /> : <Loader />}
       <Menu>
-        <MenuItem href={paths.home()}>Início</MenuItem>
-        <MenuItem href={paths.memoryCreate()}>Criar Homenagem</MenuItem>
-        <MenuItem href={paths.aboutUs()}>Quem Somos</MenuItem>
+        <MenuItem href={paths.home()}>
+          <FormattedMessage
+            id="components--menu-item.home"
+            defaultMessage="Início"
+          />
+        </MenuItem>
+        <MenuItem href={paths.memoryCreate()}>
+          <FormattedMessage
+            id="components--menu-item.create-memory"
+            defaultMessage="Criar Homenagem"
+          />
+        </MenuItem>
+        <MenuItem href={paths.aboutUs()}>
+          <FormattedMessage
+            id="components--menu-item.about-us"
+            defaultMessage="Quem Somos"
+          />
+        </MenuItem>
         <a
           className="components--menu-item"
           target="_blank"
           href="https://www.facebook.com/sharer.php?u=https://vivosemnos.org"
         >
-          Compartilhar
+          <FormattedMessage
+            id="components--menu-item.share"
+            defaultMessage="Compartilhar"
+          />
         </a>
         <a
           className="components--menu-item"
           target="_blank"
           href="https://www.facebook.com/vivosennos/?fref=ts"
         >
-          Seguir
+          <FormattedMessage
+            id="components--menu-item.follow"
+            defaultMessage="Seguir"
+          />
         </a>
       </Menu>
       <Section type={Header}>Header</Section>

b6d332eadd60895e05fa03091f8b031980d5f417
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2017-05-10 21:58:04 -0300

    chore(intl): filter featured memories bu language #165

--- src/memories/queries/memories-featured.js
+++ src/memories/queries/memories-featured.js
@@ -1,21 +1,25 @@
 import { gql } from 'react-apollo'
 
-export default gql`query {
-    allMemories(
-      first: 5,
-      orderBy: ID_DESC,
-    ) {
-      nodes {
-        id,
-        ownerFirstName,
-        victimBornAt,
-        victimDeadAt,
-        victimHistory,
-        victimName,
-        victimPhoto,
-        victimRememberText,
-        victimSilhouette,
-      }
+export default gql`
+query memoryFeaturedByLanguage(
+  $language: String!,
+) {
+  allMemories(
+    first: 5,
+    orderBy: ID_DESC,,
+    condition: { language: $language },
+  ) {
+    nodes {
+      id,
+      ownerFirstName,
+      victimBornAt,
+      victimDeadAt,
+      victimHistory,
+      victimName,
+      victimPhoto,
+      victimRememberText,
+      victimSilhouette,
     }
   }
+}
 `

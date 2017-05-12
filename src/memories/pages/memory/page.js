80ff53dd4eee58206ab1b6be6c38bac51dfb7e0d
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2017-05-12 09:46:44 -0300

    fix(memory): remove console.log #175

--- src/memories/pages/memory/page.js
+++ src/memories/pages/memory/page.js
@@ -30,143 +30,140 @@ const memoryShareText = (intl, memory) => intl.formatMessage({
   victimName: memory.victimName,
 })
 
-export default ({ memory, comments, assets, loading, intl }) => {
-  if (!loading) console.log('memoryShareURL(intl, memory)', memoryShareURL(intl, memory))
-  return (
-    <LayoutDefault>
-      <Header>
-        <TopBar />
-      </Header>
+export default ({ memory, comments, assets, loading, intl }) => (
+  <LayoutDefault>
+    <Header>
+      <TopBar />
+    </Header>
 
-      {loading ? <div /> : (
-        <div className="page pages--memory-victim">
-          <section className="section--victim-data">
-            <MemorySummary
-              name={memory.victimName}
-              owner={memory.ownerFirstName}
-              birthYear={memory.victimBornAt}
-              deathYear={memory.victimDeadAt}
-              description={memory.victimHistory}
-              image={`${process.env.SERVER_DOMAIN}${memory.victimPhoto}`}
-              imageWidth="223px"
-              imageHeight="179px"
-              imageAlignmentRight={detect.mobile ? 0 : undefined}
-              imageAlignmentLeft={!detect.mobile ? 190 : undefined}
-              baseFontSize="16px"
-              distanceY="140px"
-              width="80%"
-            />
-          </section>
+    {loading ? <div /> : (
+      <div className="page pages--memory-victim">
+        <section className="section--victim-data">
+          <MemorySummary
+            name={memory.victimName}
+            owner={memory.ownerFirstName}
+            birthYear={memory.victimBornAt}
+            deathYear={memory.victimDeadAt}
+            description={memory.victimHistory}
+            image={`${process.env.SERVER_DOMAIN}${memory.victimPhoto}`}
+            imageWidth="223px"
+            imageHeight="179px"
+            imageAlignmentRight={detect.mobile ? 0 : undefined}
+            imageAlignmentLeft={!detect.mobile ? 190 : undefined}
+            baseFontSize="16px"
+            distanceY="140px"
+            width="80%"
+          />
+        </section>
 
-          <section className="section--quote">
-            <Silhouette variation={memory.victimSilhouette} height="594" />
-            <div className="title">
-              {memory.victimGoodWords}
-            </div>
-            <Quote>
-              {memory.victimRememberText}
-            </Quote>
-          </section>
+        <section className="section--quote">
+          <Silhouette variation={memory.victimSilhouette} height="594" />
+          <div className="title">
+            {memory.victimGoodWords}
+          </div>
+          <Quote>
+            {memory.victimRememberText}
+          </Quote>
+        </section>
 
-          {!assets || !assets.length ? null : (
-            <SectionPrimary
-              className="section--gallery"
-              header={
-                <SectionHeader
-                  hideBorder
-                  title={
-                    <FormattedMessage
-                      id="pages--memory-victim.section--gallery.header"
-                      defaultValue="Galeria de imagens"
-                    />
-                  }
-                />
-              }
-            >
-              <div className="gallery columns is-multiline is-desktop">
-                {assets
-                  .filter(asset => asset.assetType === 'image')
-                  .map((asset, index) => (
-                    <div className="column column is-one-third">
-                      <img
-                        src={`${process.env.SERVER_DOMAIN}${asset.assetUrl}`}
-                        alt={`asset-${index}`}
-                      />
-                    </div>
-                  ))
-                }
-              </div>
-            </SectionPrimary>
-          )}
-
+        {!assets || !assets.length ? null : (
           <SectionPrimary
-            className="section--share"
+            className="section--gallery"
             header={
               <SectionHeader
                 hideBorder
                 title={
                   <FormattedMessage
-                    id="pages--memory-victim.section--share.header"
-                    defaultValue="Compartilhe"
+                    id="pages--memory-victim.section--gallery.header"
+                    defaultValue="Galeria de imagens"
                   />
                 }
               />
             }
           >
-            <ShareFacebookButton
-              className="share-button"
-              href={memoryShareURL(intl, memory)}
-            />
-            <ShareTwitterButton
-              className="share-button"
-              href={memoryShareURL(intl, memory)}
-              text={memoryShareText(intl, memory)}
-            />
-            {!detect.mobile ? <i /> : (
-              <ShareWhatsappButton
-                className="share-button"
-                text={`${memoryShareText(intl, memory)} ${memoryShareURL(intl, memory)}`}
-              />
-            )}
+            <div className="gallery columns is-multiline is-desktop">
+              {assets
+                .filter(asset => asset.assetType === 'image')
+                .map((asset, index) => (
+                  <div className="column column is-one-third">
+                    <img
+                      src={`${process.env.SERVER_DOMAIN}${asset.assetUrl}`}
+                      alt={`asset-${index}`}
+                    />
+                  </div>
+                ))
+              }
+            </div>
           </SectionPrimary>
+        )}
 
-          <SectionPrimary className="section--memory-comments">
-            {comments.map(comment => (
-              <div className="block--memory-comment">
-                <div className="commenter--name">
-                  {comment.name}
-                </div>
-                <div className="commenter--comment">
-                  {comment.comment}
-                </div>
+        <SectionPrimary
+          className="section--share"
+          header={
+            <SectionHeader
+              hideBorder
+              title={
+                <FormattedMessage
+                  id="pages--memory-victim.section--share.header"
+                  defaultValue="Compartilhe"
+                />
+              }
+            />
+          }
+        >
+          <ShareFacebookButton
+            className="share-button"
+            href={memoryShareURL(intl, memory)}
+          />
+          <ShareTwitterButton
+            className="share-button"
+            href={memoryShareURL(intl, memory)}
+            text={memoryShareText(intl, memory)}
+          />
+          {!detect.mobile ? <i /> : (
+            <ShareWhatsappButton
+              className="share-button"
+              text={`${memoryShareText(intl, memory)} ${memoryShareURL(intl, memory)}`}
+            />
+          )}
+        </SectionPrimary>
+
+        <SectionPrimary className="section--memory-comments">
+          {comments.map(comment => (
+            <div className="block--memory-comment">
+              <div className="commenter--name">
+                {comment.name}
               </div>
-            ))}
-          </SectionPrimary>
+              <div className="commenter--comment">
+                {comment.comment}
+              </div>
+            </div>
+          ))}
+        </SectionPrimary>
 
-          <SectionPrimary
-            id="comments"
-            className="section--known-victim"
-            header={
-              <SectionHeader
-                title={
-                  <FormattedMessage
-                    id="pages--memory-victim.section--known-victim.header"
-                    defaultValue="Quer contribuir para essa homenagem?"
-                  />
-                }
-                subtitle={
-                  <FormattedMessage
-                    id="pages--memory-victim.section--known-victim.header.subtitle"
-                    defaultValue="Conte aqui também a sua memória"
-                  />
-                }
-              />
-            }
-          >
-            <MemoryCommentsForm memoryId={memory.id} victimName={memory.victimName} />
-          </SectionPrimary>
-        </div>
-      )}
-    </LayoutDefault>
-  )
-}
+        <SectionPrimary
+          id="comments"
+          className="section--known-victim"
+          header={
+            <SectionHeader
+              title={
+                <FormattedMessage
+                  id="pages--memory-victim.section--known-victim.header"
+                  defaultValue="Quer contribuir para essa homenagem?"
+                />
+              }
+              subtitle={
+                <FormattedMessage
+                  id="pages--memory-victim.section--known-victim.header.subtitle"
+                  defaultValue="Conte aqui também a sua memória"
+                />
+              }
+            />
+          }
+        >
+          <MemoryCommentsForm memoryId={memory.id} victimName={memory.victimName} />
+        </SectionPrimary>
+      </div>
+    )}
+  </LayoutDefault>
+)
